217b68f9a8a9ebaec9ca0124de1d8d43a8e979c1
playerking3 <murilomarquesdecastro@gmail.com>
2023-05-18 17:11:54 -0300

    fiz alteraçãoes estéticas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-const aluno = {
+const calculos = {
     aritmetica: function(n1,n2,n3,n4,n5){
         let total = (n1 + n2 + n3 + n4 + n5) / 5
-        return aluno.validate(total.toFixed(2))
+        return calculos.validate(total.toFixed(2))
     },
     ponderada: function(n1,n2,n3,n4,n5,p1,p2,p3,p4,p5){
         let f1 = n1 * p1
@@ -11,22 +11,22 @@ const aluno = {
         let f5 = n5 * p5
         let divisor = p1 + p2 + p3 + p4 + p5
         total = (f1 + f2 + f3 + f4 + f5) / divisor
-        return aluno.validate(total.toFixed(2))  
+        return calculos.validate(total.toFixed(2))  
     },
     condicao: function(nota){
         if (nota >= 7){
-            return "O aluno foi aprovado com a média: "+ nota
+            return "O aluno(a) foi <spam class='sucesso'> aprovado </spam>com a média: "+ nota
         }else if(nota < 7 && nota >= 5){
-            return "O aluno está em exame com a média: "+ nota
+            return "O aluno(a) está <spam class='exam'>em exame </spam> com a média: "+ nota
         }else{
-            return "O aluno reprovou com a média: "+ nota
+            return "O aluno(a) <spam class='erro'>reprovou </spam> com a média: "+ nota
         }
     },
     validate: function(nota){
         if (nota == "NaN"){
-            return "Erro, adicione apenas números e casso houver casa decimal separar com ponto ( . )"
+            return "<spam class='erro'>Erro, adicione apenas números</spam>"
         }else{
-            return aluno.condicao(nota)
+            return calculos.condicao(nota)
         }
     }
 }
@@ -36,12 +36,12 @@ notaAritmetica.addEventListener("submit", enviarAritmetica)
 function enviarAritmetica(event){
     event.preventDefault()
     let data = new FormData(this)
-    let n1 = parseFloat((data.get("nota1")))
-    let n2 = parseFloat((data.get("nota2")))
-    let n3 = parseFloat((data.get("nota3")))
-    let n4 = parseFloat((data.get("nota4")))
-    let n5 = parseFloat((data.get("nota5")))
-    resultado.innerHTML = (aluno.aritmetica(n1,n2,n3,n4,n5))
+    let n1 = parseFloat((data.get("nota1").replace(",",".")))
+    let n2 = parseFloat((data.get("nota2").replace(",",".")))
+    let n3 = parseFloat((data.get("nota3").replace(",",".")))
+    let n4 = parseFloat((data.get("nota4").replace(",",".")))
+    let n5 = parseFloat((data.get("nota5").replace(",",".")))
+    resultado.innerHTML = (calculos.aritmetica(n1,n2,n3,n4,n5))
 }
 
 let notaPonderada = document.getElementById("form2")
@@ -49,17 +49,17 @@ notaPonderada.addEventListener("submit", enviarPonderada)
 function enviarPonderada(event){
     event.preventDefault()
     let data = new FormData(this)
-    let n1 = parseFloat((data.get("nota1")))
-    let n2 = parseFloat((data.get("nota2")))
-    let n3 = parseFloat((data.get("nota3")))
-    let n4 = parseFloat((data.get("nota4")))
-    let n5 = parseFloat((data.get("nota5")))
-    let p1 = parseFloat((data.get("peso1")))
-    let p2 = parseFloat((data.get("peso2")))
-    let p3 = parseFloat((data.get("peso3")))
-    let p4 = parseFloat((data.get("peso4")))
-    let p5 = parseFloat((data.get("peso5")))
-    resultado.innerHTML = (aluno.ponderada(n1,n2,n3,n4,n5,p1,p2,p3,p4,p5))
+    let n1 = parseFloat((data.get("nota1").replace(",",".")))
+    let n2 = parseFloat((data.get("nota2").replace(",",".")))
+    let n3 = parseFloat((data.get("nota3").replace(",",".")))
+    let n4 = parseFloat((data.get("nota4").replace(",",".")))
+    let n5 = parseFloat((data.get("nota5").replace(",",".")))
+    let p1 = parseFloat((data.get("peso1").replace(",",".")))
+    let p2 = parseFloat((data.get("peso2").replace(",",".")))
+    let p3 = parseFloat((data.get("peso3").replace(",",".")))
+    let p4 = parseFloat((data.get("peso4").replace(",",".")))
+    let p5 = parseFloat((data.get("peso5").replace(",",".")))
+    resultado.innerHTML = (calculos.ponderada(n1,n2,n3,n4,n5,p1,p2,p3,p4,p5))
 }
 
 let resultado = document.getElementById("resultado")
